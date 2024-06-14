5b9b3da645561b8a783215328d66455c8192d3af
katechoi14 <152747082+katechoi14@users.noreply.github.com>
2024-06-14 09:53:29 -0400

    Update and rename App.tsx to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { AppProps } from 'next/app';
 import styles from '../styles/style.module.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
