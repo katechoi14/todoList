4a9e5549ff7da6631274f1d3c33dfc8a3c3616c4
katechoi14 <152747082+katechoi14@users.noreply.github.com>
2024-06-14 12:38:58 -0400

    Update index.tsx to include tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import ToDoList from '../components/ToDoList';
 
-const Home: React.FC = () => {
+const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Todo App</h1>
-      <Link href="/todo">Go to the Todo list!</Link>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-serif">Welcome to the Todo App</h1>
+      <Link href="/todo" className="mt-4">Go to the Todo list!</Link>
     </div>
   );
 };
