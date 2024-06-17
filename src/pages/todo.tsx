5dd899500cf728da7220745f62f5b4d7db60f8e2
katechoi14 <152747082+katechoi14@users.noreply.github.com>
2024-06-17 17:59:45 -0400

    Update todo.tsx

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import TodoList from "../components/ToDoList";
-import styles from '../style.module.css';
 
 const TodoPage = () => {
     return (
-        <div>
-            <h1>Todo Page</h1>
+        <div className="text-center text-2xl">
+            <h1 className="font-serif">Todo Page</h1>
             <TodoList />
         </div>
     );
