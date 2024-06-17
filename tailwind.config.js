654c900f7d23e55eb0fd6a7bca10414271e3309f
katechoi14 <152747082+katechoi14@users.noreply.github.com>
2024-06-17 15:44:22 -0400

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,77 +1,40 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    "./node_modules/flowbite/**/*.js",
   ],
-  prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    colors: {
+      'sky-200': '#bae6fd',
+      'slate-200': '#e2e8f0',
+      'gray-200': '#e5e7eb',
+      'zinc-200': '#e4e4e7',
+      'neutral-200': '#e5e5e5',
+      'stone-200': '#e7e5e4',
+      'red-200': '#fecaca',
+      'red-500': '#ef4444',
+      'orange-200': '#fed7aa',
+      'green-100': '#dcfce7',
+      'blue-200': '#bfdbfe',
+      'pink-200': '#fbcfe8',
+      'pink-100': '#fce7f3',
+      'rose-100': '#ffe4e6',
+      'pinkpink': '#fecdd3', 
+      'white': '#fafaf9',
+      'black': '#020617',
+      'red-600': '#dc2626',
+      'transparent': 'transparent',
+      'blue-gray-700': 'blue-gray-700',
+      'blue-gray-50': 'blue-gray-50',
+      'blue-gray-200': 'blue-gray-200',
+      't-blue-gray-200': 't-blue-gray-200',
+      't-transparent': 't-transparent',
+      'gray-900': 'gray-900',
     },
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [require('flowbite/plugin')],
+};
