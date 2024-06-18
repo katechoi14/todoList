fa1e743916b85f2c72f935732f24940077b96fa0
katechoi14 <152747082+katechoi14@users.noreply.github.com>
2024-06-17 20:31:14 -0400

    Update ToDoList.tsx

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -97,59 +97,57 @@ const TodoList: React.FC = () => {
     setDragOverIndex(null);
   };
 
-  const inputClassNames = "peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900";
-  const labelClassNames = "flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900";
-
   return (
-<div className="flex flex-col justify-center items-center overflow-x-hidden max-w-full min-h-screen">
-  <div className="text-center">
-    <h2 className="h-20 text-xl flex items-center justify-center">Todo List</h2>
-    <form className="items-center" onSubmit={addTask}>
-      <div className="w-72">
-        <div className="relative w-full min-w-[200px] h-10">
-          <input
-            className={inputClassNames}
-            placeholder=" "
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-          />
-          <label className={labelClassNames}>
-            Task
-          </label>
-        </div>
+    <div className="flex flex-col justify-center items-center overflow-x-hidden max-w-full min-h-screen">
+      <div className="text-center">
+        <h2 className="h-20 text-xl">Todo List</h2>
+        <form onSubmit={addTask}>
+          <div className="flex flex-col gap-6 w-72">
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                id="task"
+                className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100" 
+                placeholder=" "
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+              />
+              <label htmlFor="task" className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Task
+              </label>
+            </div>
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                id="username"
+                className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100" 
+                placeholder=" "
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <label htmlFor="username" className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Username
+              </label>
+            </div>
+            <div className="relative min-w-[200px] h-10">
+              <DatePicker
+                id="selectedDate"
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
+                placeholderText="Date"
+              />
+              <label htmlFor="selectedDate" className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              </label>
+            </div>
+          </div>
+          <div className="w-72 max-w-full mt-4">
+            <button 
+              type="submit"
+              className="text-base bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              Add Task!
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="w-72 mt-6">
-        <div className="relative w-full min-w-[200px] h-10">
-          <input
-            className={inputClassNames}
-            placeholder=" "
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <label className={labelClassNames}>
-            Username
-          </label>
-        </div>
-      </div>
-      <div className="w-72 mt-4">
-        <div>
-          <DatePicker
-            id="selectedDate"
-            selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-3 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-        </div>
-      </div>
-      <div className="flex justify-center items-center w-72 max-w-full mt-4">
-        <button 
-          type="submit"
-          className="text-base bg-transparent hover:bg-pinkpink text-pinkpink font-semibold hover:text-white py-2 px-4 border border-pinkpink hover:border-transparent rounded">
-          Add Task!
-        </button>
-      </div>
-    </form>
-  </div>
   <ul className="list-none p-0 w-full max-w-full">
   {tasks.map((task, index) => (
     <li
