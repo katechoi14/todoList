860e82d01c78371a19e60174096e231f09aa1aab
katechoi14 <kchoi2@stevens.edu>
2024-07-17 17:08:31 -0400

    updated w loginform

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <h1 className="text-2xl font-serif">Welcome to the Todo App</h1>
-      <Link href="/login" className="mt-4">Log In!</Link>
+      <Link href="/loginForm" className="mt-4">Log In!</Link>
       <Link href="/todo" className="mt-4">Go to the Todo list!</Link>
     </div>
   );
--- src/pages/loginForm.tsx
+++ src/pages/loginForm.tsx
@@ -1,3 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(`${API_URL}/users/login`, {
+                email, password
+            });
+            const { token } = response.data;
+            localStorage.setItem('accessToken', token);
+        } catch (error) {
+            setError('Invalid email or password');
+        }
+    };
+
+    return (
+        <form onSubmit={handleLogin} className="space-y-12 w-full sm:w-[400px]">
+            <div className="grid w-full items-center gap-1.5">
+                <input 
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                id="email"
+                required
+            />
+            </div>
+            <div className="grid w-full items-center gap-1.5">
+                <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+            />
+            </div>
+        <button className="w-full" type="submit">Login</button>
+        {error && <p>{error}</p>}
+        </form>
+    );
+};
+
+export default LoginForm;
 
 
